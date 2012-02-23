61d715545e6cfb8639fa6d22a657f8b9628a370b
Jonathan Stray <jstray@ap.org>
2012-02-23 14:25:07 -0600

    Original PDFs are good enough, so remove ABBY OCR'd versions, saving many megabytes

---
+++ extract-text-all.sh
@@ -0,0 +1,8 @@
+#/usr/bin/sh
+docsplit text --pages all -o textpages january-june-2005.pdf
+docsplit text --pages all -o textpages june-december-2005.pdf
+docsplit text --pages all -o textpages january-to-june-2006.pdf
+docsplit text --pages all -o textpages june-to-december-2006.pdf
+docsplit text --pages all -o textpages january-to-june-2007.pdf
+docsplit text --pages all -o textpages june-to-december-2007.pdf
+
